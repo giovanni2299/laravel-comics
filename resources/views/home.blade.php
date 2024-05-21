c972f40f3ab528dd2d95c13b349233d9637f55f2
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-21 14:30:05 +0200

    Refact+ADD: style and add a "description in home"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 @extends('layout.app')
 @section('content')
-<h1 class="text-center p-3">Home</h1>
+<h1 class="text-center p-3">
+    Home 
+    <br>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, quasi! Cum molestias quisquam incidunt iste provident asperiores, consequuntur dignissimos veniam nemo omnis ullam! Dolore, voluptas a repellat similique eum sunt ducimus quis dicta tenetur, nisi modi autem. Porro ut odio doloribus deleniti. Ducimus id nostrum at provident dolorem! Error, itaque.
+</h1>
 @endsection
