97a6e8e810e45912d2423b7bc0050d24a17e749b
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-22 16:06:44 +0200

    ADD: the foreach cilce and print the stuff

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,16 @@
 @extends('layout.app')
 @section('content')
 <h1 class="text-center p-3">Comics</h1>
+<ul>
+    @foreach($comics as $comic)
+    <li>
+        {{$comic['title']}}
+        <img src="{{$comic['thumb']}}" alt="">
+        {{$comic['series']}}
+    </li>
+    
+    @endforeach
+    
+</ul>
+@dump($comics)
 @endsection
